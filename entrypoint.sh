6f73b4b71ad1dca34a69a5405e8f22ede399c30e
Raz Turgeman <Raz.Turgeman97@gmail.com>
2025-02-19 00:02:34 +0000

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-# Clean any previously built files (optional, based on your needs)
+# Clean any previously built files
 echo "Cleaning previous build files..."
-rm -rf /workspace/build/*
-rm -rf /workspace/install/*
-rm -rf /workspace/log/*
+rm -rf /root/ros2_ws/build/*
+rm -rf /root/ros2_ws/install/*
+rm -rf /root/ros2_ws/log/*
 
 # Source ROS 2 setup.bash (if not already sourced)
-if [ -f "/opt/ros/humble/setup.bash" ]; then
-    source /opt/ros/humble/setup.bash
-    
-    # Source the workspace setup.bash
-    source /root/ros2_ws/install/setup.bash
-else
-    echo "ROS 2 setup.bash not found!"
-fi
+source /opt/ros/humble/setup.bash
+
+# Source the workspace setup.bash
+source /root/ros2_ws/install/setup.bash
 
 # Execute the command passed to the container (e.g., bash or a specific command)
 exec "$@"
-
